a1bac45f2f62c2084d12b28857565068ebcaec91
Joe Issac III <mreyeballs29@gmail.com>
2020-04-21 17:14:02 -0500

    Removed 1.12 Content

--- src/main/java/com/mreyeballs29/issactncore/init/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * <p>
- * This package allows to register some features such as currently
- * blocks and items. Since it is a core mod, it does neither needed
- * enities, biomes, nor dimensions.
- */
-package com.mreyeballs29.issactncore.init;
--- src/main/java/com/mreyeballs29/issactncore/util/Periodaurus.java
+++
@@ -1,7 +0,0 @@
-package com.mreyeballs29.issactncore.util;
-
-/**
- * The <code>Periodaurus</code> is a instantiable class that allows the item stacks to register as long as the
- * element of the key map is presented upon parse the string.
- */
-public class Periodaurus {}
